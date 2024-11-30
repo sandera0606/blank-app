8af4f8425c12bb307a7457eb9cb97f74d6fffab2
WaterOuttaFish <joanna.rain.wang@gmail.com>
2024-11-30 16:43:36 -0500

    updated store urls for webscrapping

--- scripts/webscraping.py
+++ scripts/webscraping.py
@@ -1,19 +1,44 @@
 import requests #allows for https requests 
-import beautifulsoup #html parsing 
+from bs4 import beautifulsoup #html parsing 
 import re 
 
 store_names = [] #will be the store name like costco, metro, t&t, etc
 products = [] #
 
 random_locations = ["costco", "walmart", "t&t"]
-random_products = ["eggs", "beef", "yep"]
+random_products = ["eggs", "beef", "milk"]
 
-#walmart price parsing: <span class="w_q67L">current price $6.88</span>
+#current price 
 
 product = "" #insert the same of the products 
 
 #t&t: (f"https://www.tntsupermarket.com/eng/search.html?query={product}") 
 #filter by item-price-zRu
 #<div class="item-price-zRu"><span>$</span><span>12</span><span>.</span><span>99</span></div>
-for store in store_names:
-<a link-identifier="6000191268613" class="w-100 h-100 z-1 hide-sibling-opacity  absolute" target="" href="/en/ip/Gray-Ridge-Premium-Large-White-Eggs/6000191268613?selectedSellerId=0&amp;from=/search"><span class="w_q67L">Gray Ridge Premium Large White Eggs, 18 Count </span></a>
+
+#metro: (f"https://www.metro.ca/en/online-grocery/search?filter={product}")
+
+#walmart: (f"https://www.walmart.ca/en/search?q={product}")
+
+#loblaws: (f"https://www.loblaws.ca/search?search-bar={product}")
+
+#nofrills: (f"https://www.nofrills.ca/search?search-bar={product}")
+
+
+url_dict = {
+    "Walmart": "https://www.walmart.ca/en/search?q="+product,
+    "Metro": "https://www.metro.ca/en/online-grocery/search?filter="+product,
+    "Loblaws": "https://www.loblaws.ca/search?search-bar="+product,
+    "No Frills": "https://www.nofrills.ca/search?search-bar="+product,
+    "T&T Supermarket": "https://www.tntsupermarket.com/eng/search.html?query="+product
+}
+
+stores = ["Walmart", "Metro", "Loblaws", "No Frills", "T&T Supermarket"]
+
+def parse_data(stores, product, url):
+    for store in stores: 
+        if store == url.dict.values():
+            pass 
+
+def get_
+for store, url in url_dict.items():
