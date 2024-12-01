b9c6da9f6fdc7f4412c0d056b73c9a82d9e9806d
sandera <shuang2322@gmail.com>
2024-12-01 10:35:35 -0500

    adkfjadf

--- scripts/webscraper.py
+++ scripts/webscraper.py
@@ -72,7 +72,7 @@ def parse_data(store, product):
 
     manage_cookies(driver, f"{store}_cookies.pkl")
 
-    WebDriverWait(driver, 4).until(
+    WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located((By.CLASS_NAME, selector_dict[store]))
     )
 
